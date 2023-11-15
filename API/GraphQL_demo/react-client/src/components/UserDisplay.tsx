4730a8e4e259b926ace2a7bdeb103ed5a5524083
Sacha924 <24448@student.dorset-college.ie>
2023-11-15 10:51:45 +0100

    end of basic UI with clean code

--- API/GraphQL_demo/react-client/src/components/UserDisplay.tsx
+++ API/GraphQL_demo/react-client/src/components/UserDisplay.tsx
@@ -1,17 +1,24 @@
 import React from 'react'
 import { User } from '../types'
+import MessageDisplay from './MessageDisplay'
 
+type Props = {
+    user: User
+}
 
-export default function UserDisplay(user: User) {
+function UserDisplay({ user }: Props) {
     return (
-        <div className="text-white-600 flex-col  flex items-center justify-center p-4 gap-y-12 border border-sky-500">
-
-            <p>{user.name}</p>
-            {user.messages.map((message) => {
-                return (
-                    <div>{message.body}</div>
-                )
-            })}
+        <div className="flex gap-x-24 justify-center">
+            <div className="rounded-sm flex justify-center items-center drop-shadow-md bg-neutral-700 w-48 h-20">
+                <p className="text-xl text-gray-200 font-bold">
+                    {user.name}
+                </p>
+            </div>
+            <div>
+                {user.messages.map((message, i) => <MessageDisplay key={i} index={i} message={message} />)}
+            </div>
         </div>
     )
 }
+
+export default UserDisplay
