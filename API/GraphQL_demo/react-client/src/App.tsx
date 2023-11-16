9303d13630c932ed1b1c94810e91d71dcf7bb0fc
Sacha924 <24448@student.dorset-college.ie>
2023-11-16 18:13:40 +0100

    End Project

--- API/GraphQL_demo/react-client/src/App.tsx
+++ API/GraphQL_demo/react-client/src/App.tsx
@@ -1,26 +1,19 @@
 import { User } from './types'
 import UserDisplay from './components/UserDisplay'
+import { useQuery } from 'urql'
+import { GetUsersDocument } from './graphql/generated'
+
 
 function App() {
-  const users: User[] = [{
-    name: "Sacha",
-    messages: [{
-      body: 'Prisma rocks!!'
-    }, {
-      body: 'Did I mention I love Prisma?'
-    }]
-  },
-  {
-    name: "Jules",
-    messages: [{
-      body: 'Yo le gang'
-    }]
-  }]
+  const [results] = useQuery({
+    query: GetUsersDocument
+  })
+
 
   return (
     <div className="bg-zinc-800 flex-col h-screen w-full flex items-center justify-center p-4 gap-y-12 overflow-scroll">
       {
-        users.map((user, i) => <UserDisplay user={user} key={i}/>)
+        results.data?.users.map((user, i) => <UserDisplay user={user} key={i}/>)
       }
     </div>
   )
