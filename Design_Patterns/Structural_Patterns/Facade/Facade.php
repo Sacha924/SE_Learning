d32da8c5781edbb423c24621156216f01877374d
Sacha924 <24448@student.dorset-college.ie>
2023-11-26 20:22:21 +0100

    Flyweight and Proxy (less used structural pattern)

---
+++ Design_Patterns/Structural_Patterns/Facade/Facade.php
@@ -0,0 +1,70 @@
+// Taking our computer example from above. Here we have the computer class
+
+class Computer
+{
+    public function getElectricShock()
+    {
+        echo "Ouch!";
+    }
+
+    public function makeSound()
+    {
+        echo "Beep beep!";
+    }
+
+    public function showLoadingScreen()
+    {
+        echo "Loading..";
+    }
+
+    public function bam()
+    {
+        echo "Ready to be used!";
+    }
+
+    public function closeEverything()
+    {
+        echo "Bup bup bup buzzzz!";
+    }
+
+    public function sooth()
+    {
+        echo "Zzzzz";
+    }
+
+    public function pullCurrent()
+    {
+        echo "Haaah!";
+    }
+}
+Here we have the facade
+
+class ComputerFacade
+{
+    protected $computer;
+
+    public function __construct(Computer $computer)
+    {
+        $this->computer = $computer;
+    }
+
+    public function turnOn()
+    {
+        $this->computer->getElectricShock();
+        $this->computer->makeSound();
+        $this->computer->showLoadingScreen();
+        $this->computer->bam();
+    }
+
+    public function turnOff()
+    {
+        $this->computer->closeEverything();
+        $this->computer->pullCurrent();
+        $this->computer->sooth();
+    }
+}
+Now to use the facade
+
+$computer = new ComputerFacade(new Computer());
+$computer->turnOn(); // Ouch! Beep beep! Loading.. Ready to be used!
+$computer->turnOff(); // Bup bup buzzz! Haah! Zzzzz
