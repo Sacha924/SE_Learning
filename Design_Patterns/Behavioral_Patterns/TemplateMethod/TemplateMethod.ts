da4a069f002609d0fe164c2920d3f9dd8d2e877c
Sacha924 <24448@student.dorset-college.ie>
2023-12-04 12:18:04 +0100

    Visitor

--- Design_Patterns/Behavioral_Patterns/TemplateMethod/TemplateMethod.ts
+++ Design_Patterns/Behavioral_Patterns/TemplateMethod/TemplateMethod.ts
@@ -88,15 +88,15 @@ class ConcreteClass2 extends AbstractClass {
  * code does not have to know the concrete class of an object it works with, as
  * long as it works with objects through the interface of their base class.
  */
-function clientCode(abstractClass: AbstractClass) {
-    // ...
-    abstractClass.templateMethod();
-    // ...
-}
+// function clientCode(abstractClass: AbstractClass) {
+//     // ...
+//     abstractClass.templateMethod();
+//     // ...
+// }
 
-console.log('Same client code can work with different subclasses:');
-clientCode(new ConcreteClass1());
-console.log('');
+// console.log('Same client code can work with different subclasses:');
+// clientCode(new ConcreteClass1());
+// console.log('');
 
-console.log('Same client code can work with different subclasses:');
-clientCode(new ConcreteClass2());
+// console.log('Same client code can work with different subclasses:');
+// clientCode(new ConcreteClass2());
