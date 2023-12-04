fe1bd849a0d70aecdb282c3ce780e59b3219a1ea
Sacha924 <24448@student.dorset-college.ie>
2023-12-04 10:22:39 +0100

    strategy

--- Design_Patterns/Behavioral_Patterns/Strategy/Strategy.js
+++ Design_Patterns/Behavioral_Patterns/Strategy/Strategy.js
@@ -1,82 +1,62 @@
 /**
  * The Context defines the interface of interest to clients.
  */
- class Contexttt {
-    /**
-     * @type {Strategy} The Context maintains a reference to one of the Strategy
-     * objects. The Context does not know the concrete class of a strategy. It
-     * should work with all strategies via the Strategy interface.
-     */
-    private strategy: Strategy;
-
+var Contexttt = /** @class */ (function () {
     /**
      * Usually, the Context accepts a strategy through the constructor, but also
      * provides a setter to change it at runtime.
      */
-    constructor(strategy: Strategy) {
+    function Contexttt(strategy) {
         this.strategy = strategy;
     }
-
     /**
      * Usually, the Context allows replacing a Strategy object at runtime.
      */
-    public setStrategy(strategy: Strategy) {
+    Contexttt.prototype.setStrategy = function (strategy) {
         this.strategy = strategy;
-    }
-
+    };
     /**
      * The Context delegates some work to the Strategy object instead of
      * implementing multiple versions of the algorithm on its own.
      */
-    public doSomeBusinessLogic(): void {
+    Contexttt.prototype.doSomeBusinessLogic = function () {
         // ...
-
         console.log('Context: Sorting data using the strategy (not sure how it\'ll do it)');
-        const result = this.strategy.doAlgorithm(['a', 'b', 'c', 'd', 'e']);
+        var result = this.strategy.doAlgorithm(['a', 'b', 'c', 'd', 'e']);
         console.log(result.join(','));
-
         // ...
-    }
-}
-
-/**
- * The Strategy interface declares operations common to all supported versions
- * of some algorithm.
- *
- * The Context uses this interface to call the algorithm defined by Concrete
- * Strategies.
- */
-interface Strategy {
-    doAlgorithm(data: string[]): string[];
-}
-
+    };
+    return Contexttt;
+}());
 /**
  * Concrete Strategies implement the algorithm while following the base Strategy
  * interface. The interface makes them interchangeable in the Context.
  */
-class ConcreteStrategyAAAA implements Strategy {
-    public doAlgorithm(data: string[]): string[] {
+var ConcreteStrategyAAAA = /** @class */ (function () {
+    function ConcreteStrategyAAAA() {
+    }
+    ConcreteStrategyAAAA.prototype.doAlgorithm = function (data) {
         return data.sort();
+    };
+    return ConcreteStrategyAAAA;
+}());
+var ConcreteStrategyB = /** @class */ (function () {
+    function ConcreteStrategyB() {
     }
-}
-
-class ConcreteStrategyB implements Strategy {
-    public doAlgorithm(data: string[]): string[] {
+    ConcreteStrategyB.prototype.doAlgorithm = function (data) {
         return data.reverse();
-    }
-}
-
+    };
+    return ConcreteStrategyB;
+}());
 /**
  * The client code picks a concrete strategy and passes it to the context. The
  * client should be aware of the differences between strategies in order to make
  * the right choice.
  */
-const contextttttt = new Contexttt(new ConcreteStrategyAAAA());
+var contexttt = new Contexttt(new ConcreteStrategyAAAA());
 console.log('Client: Strategy is set to normal sorting.');
-contextttttt.doSomeBusinessLogic();
-
+contexttt.doSomeBusinessLogic();
 console.log('');
-
 console.log('Client: Strategy is set to reverse sorting.');
-contextttttt.setStrategy(new ConcreteStrategyB());
-contextttttt.doSomeBusinessLogic();
+contexttt.setStrategy(new ConcreteStrategyB());
+contexttt.doSomeBusinessLogic();
