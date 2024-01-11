b813c1aa23a086110df2ec18f077ad318488781c
Sacha924 <24448@student.dorset-college.ie>
2024-01-11 12:01:39 +0100

    Add system to save message (like a db but just a file)

--- Real_Time_Data/WebSockets/server/index.js
+++ Real_Time_Data/WebSockets/server/index.js
@@ -1,36 +1,51 @@
 // Import the required libraries
 const WebSocket = require('ws');
 const http = require('http');
+const fs = require('fs');
+
+const messagesFile = 'data.txt';
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('WebSocket server is running');
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('WebSocket server is running');
 });
 
 const wss = new WebSocket.Server({ server });
 
 wss.on('connection', (ws) => {
-  console.log('New client connected!');
+    console.log('New client connected!');
+    ws.send('Welcome to the chat!');
 
-  ws.send('Welcome to the chat!');
-
-
-  ws.on('message', (message) => {
-    console.log(`Received message from client: ${message}`);
-
-    wss.clients.forEach((client) => {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(message);
-      }
+    fs.readFile(messagesFile, { encoding: 'utf-8' }, (err, data) => {
+        if (!err) {
+            const messages = data.split('\n').filter(line => line);
+            messages.forEach(message => ws.send(message));
+        }
     });
-  });
 
-  ws.on('close', () => {
-    console.log('Client has disconnected');
-  });
+
+    ws.on('message', (message) => {
+        console.log(`Received message from client: ${message}`);
+
+        fs.appendFile(messagesFile, message + '\n', err => {
+            if (err) console.error('Error writing to file', err);
+        });
+
+        wss.clients.forEach((client) => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(message);
+            }
+        });
+
+
+    });
+
+    ws.on('close', () => {
+        console.log('Client has disconnected');
+    });
 });
 
 
 server.listen(8080, () => {
-  console.log('Server is listening on port 8080');
+    console.log('Server is listening on port 8080');
 });
