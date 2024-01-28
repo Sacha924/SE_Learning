9defcc028badad2110cbfec098fee2e75e0f11cb
Sacha924 <24448@student.dorset-college.ie>
2024-01-28 19:59:55 +0100

    channels test

--- Golang/first_test/zz_tests.go
+++ Golang/first_test/zz_tests.go
@@ -27,31 +27,77 @@ package main
 
 import (
     "fmt"
-    "sync"
-    "time"
+    // "sync"
+    // "time"
 )
 
-var wg sync.WaitGroupclear
+// var wg sync.WaitGroupclear
 
-func run(name string) {
-    defer wg.Done()
-    for i := 0; i < 3; i++ {
-        time.Sleep(1 * time.Second)
-        fmt.Println(name, " : ", i)
-    }
+// func run(name string) {
+//     defer wg.Done()
+//     for i := 0; i < 3; i++ {
+//         time.Sleep(1 * time.Second)
+//         fmt.Println(name, " : ", i)
+//     }
+// }
+
+// func main() {
+//     debut := time.Now()
+
+//     wg.Add(1)
+//     go run("Hatim")
+//     wg.Add(1)
+//     go run("Robert")
+//     wg.Add(1)
+//     go run("Alex")
+
+//     wg.Wait()
+//     fin := time.Now()
+//     fmt.Println(fin.Sub(debut))
+// }
+
+// func send(emet chan<-int, value int){
+//     fmt.Println("send a value in the canal")
+//     emet <- value
+// }
+
+
+// func read(receive <-chan int) int{
+//     fmt.Println("read a value in the canal")
+//     val := <- receive
+//     return val
+// }
+
+// func main() {
+//     fmt.Println("a")
+
+//     canal := make(chan int)
+//     go send(canal,  42)
+//     val := read(canal)
+//     fmt.Println(val)
+    
+// }
+
+func send(emet chan<- int, value int) {
+    fmt.Println("send a value in the canal")
+    emet <- value
+}
+
+func read(receive <-chan int, returnChan chan<- int) {
+    fmt.Println("read a value in the canal")
+    val := <-receive
+    returnChan <- val
 }
 
 func main() {
-    debut := time.Now()
+    fmt.Println("a")
 
-    wg.Add(1)
-    go run("Hatim")
-    wg.Add(1)
-    go run("Robert")
-    wg.Add(1)
-    go run("Alex")
+    canal := make(chan int)
+    returnChan := make(chan int)
 
-    wg.Wait()
-    fin := time.Now()
-    fmt.Println(fin.Sub(debut))
+    go send(canal, 42)
+    go read(canal, returnChan)
+
+    val := <-returnChan
+    fmt.Println(val)
 }
