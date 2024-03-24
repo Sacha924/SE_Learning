b7a1f68d68816b1f6342021d00ef1bc31ef6d644
Sacha924 <24448@student.dorset-college.ie>
2024-03-24 11:37:41 +0100

    Faking User Agent

---
+++ RootMe/script.py
@@ -0,0 +1,11 @@
+import requests
+ 
+reponse = requests.get(
+    'http://challenge01.root-me.org/web-serveur/ch2/index.php',
+    headers={'User-Agent': 'admin'}
+    )
+ 
+if reponse.status_code == 200:
+    print(reponse.text)
+else:
+    print('Error')
